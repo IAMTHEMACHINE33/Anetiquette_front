2b0db50f3c267c38433a8fe4d4250169a7f01cf5
Eric Shrestha <63570381+Chewbacapaca@users.noreply.github.com>
2022-11-13 21:18:14 +0545

    backend commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,120 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import * as style from '../style';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    clearErrors,
+    login,
+    register,
+} from "../actions/userActions"
+import {useAlert} from "react-alert"
 
 
 const Login=()=> {
-        const navigate = useNavigate();
-        const [signIn, toggle] = React.useState(true);
+    const location = useLocation()
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
+    const alert = useAlert()
+
+    const {error, loading, isAuthenticated} = useSelector((state)=>state.user)
+
+     const [loginEmail, setLoginEmail] = useState("")
+     const [loginPassword, setLoginPassword]= useState("")
+
+     const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+     })
+
+     const {name, email, password} = user
+
+
+     const loginSubmit=(e)=>{
+        e.preventDefault();
+        dispatch(login(loginEmail, loginPassword))
+     }
+
+     const registerSubmit=(e)=>{
+        e.preventDefault()
+        const myForm = new FormData()
+
+        myForm.set("name", name)
+        myForm.set("email", email)
+        myForm.set("password", password)
+        
+        dispatch(register(myForm))
+     }
+
+     const registerDataChange = (e)=>{
+            setUser({...user, [e.target.name]: e.target.value})
+     }
+
+     const redirect=location.search ? location.search.split("=")[1] : "/"
+
+     useEffect(() => {
+        if(error){
+            alert.error(error)
+            dispatch(clearErrors())
+        }
+        if(isAuthenticated){
+            navigate(redirect)
+        }
+     }, [dispatch, error, navigate, isAuthenticated, redirect])
+
+    const [signIn, toggle] = React.useState(true);
       return(
           <style.Container>
-              <style.SignUpContainer signinIn={signIn}>
-                  <style.Form>
+              <style.SignUpContainer>
+                  <style.Form encType='multipart/form-data' onSubmit={registerSubmit}>
                       <style.Title>Create Account</style.Title>
-                      <style.Input type='text' placeholder='Name' />
-                      <style.Input type='email' placeholder='Email' />
-                      <style.Input type='password' placeholder='Password' />
-                      <style.Button>Sign Up</style.Button>
+                      <style.Input 
+                        type='text' 
+                        placeholder='Name' 
+                        required
+                        name="name"
+                        value={name}
+                        onChange={registerDataChange}
+                        />
+                      <style.Input 
+                      type='email' 
+                      placeholder='Email' 
+                      required
+                      name='email'
+                      value={email}
+                      onChange={registerDataChange}
+                      />
+                      <style.Input 
+                      type='password' 
+                      placeholder='Password' 
+                      required
+                      name="password"
+                      value={password}
+                      onChange={registerDataChange}
+                      />
+                      <style.Input type="submit" value="Sign Up"/>
                   </style.Form>
               </style.SignUpContainer>
 
               <style.SignInContainer signinIn={signIn}>
-                   <style.Form>
+                   <style.Form onSubmit={loginSubmit}>
                        <style.Title>Sign in</style.Title>
-                       <style.Input type='email' placeholder='E-mail' />
-                       <style.Input type='password' placeholder='Password' />
+                       <style.Input 
+                            type='email' 
+                            placeholder='E-mail' 
+                            required 
+                            value={loginEmail}
+                            onChange={(e)=>setLoginEmail(e.target.value)}
+                        />
+                       <style.Input 
+                            type='password' 
+                            placeholder='Password'
+                            required
+                            value={loginPassword} 
+                            onChange={(e)=>setLoginPassword(e.target.value)}
+                        />
                        <style.Anchor >Forgot your password?</style.Anchor>
-                       <style.Anchor ><style.Button onClick={() => navigate("/")}>Sign In</style.Button></style.Anchor>
+                       <style.Anchor ><style.Input type="submit" value="Sign In"/></style.Anchor>
                    </style.Form>
               </style.SignInContainer>
 
