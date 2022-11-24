53b3f3bc12882af7ce7c16ed62f89cade556bb4c
JA1ter <bineshdangol07@gmail.com>
2022-11-24 13:13:08 +0545

    userprofile updated

--- src/components/userprofile.js
+++ src/components/userprofile.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import Navbar from "../common/Navbar";
-import {useNavigate} from 'react-router-dom';
-import "../profile.css";
+import Navbar from "./common/Navbar";
+import "../components/profile.css";
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 export default function Profile() {
-  const navigate = useNavigate();
-
-  const Edit_Profile = () => {
-    navigate('/Edit_profile');
-  };
   return (
     <>
     <Navbar />
     <div className="gradient-custom-2" style={{ backgroundColor: '#9de2ff' }}>
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol lg="50" xl="10">
+          <MDBCol lg="9" xl="7">
             <MDBCard>
               <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '250px' }}>
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '200px' }}>
                   <MDBCardImage src="https://sportshub.cbsistatic.com/i/2022/10/16/5b842454-5953-4125-899f-c21753fdedec/chainsaw-man-makima-anime.jpg"
-                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                  <MDBBtn color='light' className="mt-2" 
-                  style={{height: '50px', overflow: 'visible',  }}
-                  onClick={Edit_Profile}>
+                    alt="Placeholder" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                  <MDBBtn outline color="dark" style={{height: '50px', overflow: 'visible'}}>
                     Edit profile
                   </MDBBtn>
                 </div>
-                <div className="ms-5" style={{ marginTop: '150px' }}>              
-                  <MDBTypography tag="h5">Makima <i class="fas fa-check"></i> </MDBTypography>
-        
+                <div className="ms-3" style={{ marginTop: '130px' }}>
+                  <MDBTypography tag="h5">Makima</MDBTypography>
                   <MDBCardText>Chainsaw Man</MDBCardText>
-
                 </div>
               </div>
               <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
@@ -48,8 +38,8 @@ export default function Profile() {
               </div>
               <MDBCardBody className="text-black p-4">
                 <div className="mb-5">
-                  <p className="lead fw-normal mb-1">About You</p>
-                  <div className="p-4" style={{ backgroundColor: '#E4DEDD' }}>
+                  <p className="lead fw-normal mb-1">About</p>
+                  <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
                     <MDBCardText className="font-italic mb-1"></MDBCardText>
                     <MDBCardText className="font-italic mb-1">Fictional</MDBCardText>
                     <MDBCardText className="font-italic mb-0">Character</MDBCardText>
