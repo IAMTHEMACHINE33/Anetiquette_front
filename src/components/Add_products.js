fa514b1153d5574dc727a9e05ca2501e33c204bf
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-11-26 16:05:12 +0545

    add product API integration

--- src/components/Add_products.js
+++ src/components/Add_products.js
@@ -1,6 +1,55 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AddProduts = () => {
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+  const [image, setImage] = useState('');
+  const [details, setDetails] = useState([]);
+
+  const config = {
+    headers:{
+        Authorization : "Bearer "+ localStorage.getItem('token')
+    }
+}
+
+  const productAdd=(e)=>{
+    e.preventDefault();
+
+    // const data ={
+    //   name: name,
+    //   price: price,
+    //   description: description,
+    //   category: category,
+    // };
+
+    const data = new FormData();
+    data.append("name",name);
+    data.append("price",price);
+    data.append("description",description);
+    data.append("category",category);
+    data.append("product_img",image);
+    axios.post("http://localhost:4000/product/add", data, config)
+    .then((response)=>{
+      console.log(response);
+      console.log(config);
+    })
+    .catch(e=>{
+      console.log(e);
+    })
+  };
+
+  useEffect(()=>{
+    axios.get("http://localhost:4000/category/show")
+    .then((response)=>{
+      console.log(response.data.data)
+      setDetails(response.data.data)
+    })
+  },[])
+
   return (
     <>
       <div className="container mt-2">
@@ -11,22 +60,23 @@ const AddProduts = () => {
           <form>
             <div class="form-group mt-2 text-secondary">
               <label for="FormControlInput1">Product name</label>
-              <input type="text" class="form-control"></input>
+              <input type="text" class="form-control" onChange={(e)=>{setName(e.target.value)}}></input>
             </div>
 
             <div class="form-group mt-2 text-secondary">
               <label for="FormControlInput1">Price</label>
-              <input type="number" class="form-control"></input>
+              <input type="number" class="form-control" onChange={(e)=>{setPrice(e.target.value)}}></input>
             </div>
 
             <div class="form-group mt-2 text-secondary">
               <label for="FormControlSelect1">Category</label>
-              <select class="form-control" id="FormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+              <select class="form-control" id="FormControlSelect1" onChange={(e)=>{setCategory(e.target.value)}}>
+                <option>Select Category</option>
+                {details.map(option=>{
+                  return(
+                    <option value={option._id}>{option.name}</option>
+                  )
+                })}
               </select>
             </div>
 
@@ -37,6 +87,7 @@ const AddProduts = () => {
                 placeholder="Write the product information"
                 id="FormControlTextarea1"
                 rows="3"
+                onChange={(e)=>{setDescription(e.target.value)}}
               ></textarea>
             </div>
 
@@ -46,11 +97,12 @@ const AddProduts = () => {
                 type="file"
                 class="form-control-file mt-2 mb-2 d-flex justify-content-center"
                 id="FormControlFile1"
+                onChange={(e)=>{setImage(e.target.files[0])}}
               ></input>
             </div>
           </form>
 
-          <button type="button" class="btn btn-success mt-3">
+          <button type="button" class="btn btn-success mt-3" onClick={productAdd}>
             Submit
           </button>
         </div>
