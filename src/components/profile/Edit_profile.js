57faacd60b2303477e784c8ad7e4b97ef6df8a43
Dipendrachy <chysunnygamer9@gmail.com>
2022-12-05 10:59:26 +0545

    edit profile form updated

--- src/components/profile/Edit_profile.js
+++ src/components/profile/Edit_profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import { Link, useNavigate, useLocation } from "react-router-dom";
 // import "../styles.css";
 import "./edit-profile.css";
@@ -6,7 +6,7 @@ import axios from "axios";
 import Footer from "../common/Footer";
 import NavigateBlack from "../common/navblack";
 
-const Edit_profile = () => {
+const Edit_profile = () => {  
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -63,74 +63,36 @@ const Edit_profile = () => {
     console.log(file);
   };
   return (
-    <div className="edit-profile-container-fluid">
-      <NavigateBlack />
-
-      <div className="">
-        <h1 className="edit__title p-2">Edit Profile</h1>
-        <hr />
-        <div className="edit-profile__content">
-          <div className="edit_left">
-            <div className="left__content">
-              <div className="edit_avatar">
-                <img src={file} />
+    <>
+    <NavigateBlack/>
+    <div className="main">
+    <div className="container-fluid p-3">
+      <div className="container w-50 outbox rounded bg-white edit-profile">
+          <div className="row justify-content-center mt-5">
+              <div className="col-md-3 border-right">
+                  <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" src="https://i.imgur.com/O1RmJXT.jpg" width="90"/><span className="font-weight-bold">{name}</span><span className="text-black-50">{email}</span></div>
               </div>
-              <p className="upload">Monika</p>
-              <input type="file" className="file" onChange={changePic} />
-            </div>
+              <div className="col-md-5 border-right">
+                  <div className="p-3 py-5">
+                      <div className="d-flex justify-content-between align-items-center mb-3">
+                          <h6 className="text-right">Edit your profile</h6>
+                      </div>
+                      <div className="row mt-3">
+                          <div className="col-md-12"><label className="labels">Name</label><input type="text" className="form-control" placeholder={name} onChange={(e)=>setName(e.target.value)}/></div>
+                          <div className="col-md-12"><label className="labels">Email</label><input type="email" className="form-control" placeholder={email} onChange={(e)=>setEmail(e.target.value)}/></div>
+                          <div className="col-md-12"><label className="labels">Password</label><input type="password" className="form-control" placeholder={password} onChange={(e)=>setPassword  (e.target.value)}/></div>
+                      </div>
+                      <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="submit" onClick={userUpdate}>Save Profile</button></div>
+                  </div>
+              </div>
+          
           </div>
-          <div className="edit_right">
-            <h3 className="personal-info"> Personal Info</h3>
-            <form>
-              <div className="edit-control">
-                <label for="fname">Name</label>
-                <input
-                  id="fname"
-                  type="text"
-                  placeholder="John wick"
-                  required
-                  value={name}
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                />{" "}
-                <br />
-              </div>
-              <div className="edit-control">
-                <label for="lname">Email</label>
-                <input
-                  id="lname"
-                  type="email"
-                  placeholder="test@gmail.com"
-                  required
-                  value={email}
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />{" "}
-                <br />
-              </div>
-
-              <div className="edit-control">
-                <label for="email">Password</label>
-                <input
-                  id="email"
-                  type="password"
-                  placeholder="*****"
-                  required
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />{" "}
-                <br />
-              </div>
-              <input type="submit" onClick={userUpdate} />
-            </form>
-          </div>
-        </div>
       </div>
-      <Footer />
     </div>
+    <Footer />
+    </div>
+    
+    </>
   );
 };
 
