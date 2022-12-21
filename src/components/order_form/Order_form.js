5d145a7718f662c65a1604462993511e72c9d1db
Dipendrachy <chysunnygamer9@gmail.com>
2022-12-21 12:36:29 +0545

    order_form re-designed

--- src/components/order_form/Order_form.js
+++ src/components/order_form/Order_form.js
@@ -96,24 +96,6 @@ const OrderForm = () => {
                   </div>
                 </div>
 
-                <div class="mb-3">
-                  <label for="username">Username</label>
-                  <div class="input-group">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">@</span>
-                    </div>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="username"
-                      placeholder="Username"
-                      required
-                    />
-                    <div class="invalid-feedback">
-                      Your username is required.
-                    </div>
-                  </div>
-                </div>
 
                 <div class="mb-3">
                   <label for="email">
@@ -144,7 +126,7 @@ const OrderForm = () => {
                   </div>
                 </div>
 
-                <div class="mb-3">
+                <div class="mb-5">
                   <label for="phoneno">
                     Phone no. <span class="text-muted"></span>
                   </label>
@@ -156,134 +138,30 @@ const OrderForm = () => {
                   />
                 </div>
 
-                <div class="row">
-                  <div class="col-md-4 mb-3">
-                    <label for="district">Country</label>
-                    <select class="form-control" id="district" required>
-                      <option value="">Choose</option>
-                      <option value="country">Nepal</option>
-                    </select>
+                
+                <hr class="mb-4" />
 
-                    <div class="invalid-feedback">State is required.</div>
-                  </div>
-                  <div class="col-md-4 mb-3">
-                    <label for="district">State</label>
-                    <select class="form-control" id="district" required>
-                      <option value="">Choose</option>
-                      <option value="kathmandu">Kathmandu</option>
-                    </select>
+                <h4 class="mb-4">Payment</h4>
 
-                    <div class="invalid-feedback">State is required.</div>
+                
+                
+                <div class="col">
+                  <div class="col-md-1 mb-2 khalti-container">
+                   {/* for khalti */}
+                  <a href="https://web.khalti.com/#/" >
+                  <img src="https://seeklogo.com/images/K/khalti-logo-F0B049E67E-seeklogo.com.png" className="khalti" alt="khalti"/>
+                  </a>
+
                   </div>
-                  <div class="col-md-3 mb-3">
-                    <label for="zip">Zip</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="zip"
-                      placeholder=""
-                      required
-                    />
-                    <div class="invalid-feedback">Zip code required.</div>
+                  <div class="col-md-1 mb-2 Cash-container">
+                    {/* for cash */}
+                  <a href="https://www.youtube.com/results?search_query=dream+on" >
+                    <img src="https://icons.veryicon.com/png/o/business/business-style-icon/wallet-62.png" className="Cash" alt="khalti"/>
+                  </a>
                   </div>
                 </div>
                 <hr class="mb-4" />
-
-                <h4 class="mb-3">Payment</h4>
-
-                <div class="d-block my-3">
-                  <div class="custom-control custom-radio">
-                    <input
-                      id="credit"
-                      name="paymentMethod"
-                      type="radio"
-                      class="custom-control-input"
-                      checked
-                      required
-                    />
-                    <label class="custom-control-label" for="credit">
-                      Credit card
-                    </label>
-                  </div>
-                  <div class="custom-control custom-radio">
-                    <input
-                      id="debit"
-                      name="paymentMethod"
-                      type="radio"
-                      class="custom-control-input"
-                      required
-                    />
-                    <label class="custom-control-label" for="debit">
-                      Debit card
-                    </label>
-                  </div>
-                  <div class="custom-control custom-radio">
-                    <input
-                      id="paypal"
-                      name="paymentMethod"
-                      type="radio"
-                      class="custom-control-input"
-                      required
-                    />
-                    <label class="custom-control-label" for="paypal">
-                      Paypal
-                    </label>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-6 mb-3">
-                    <label for="cc-name">Name on card</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="cc-name"
-                      placeholder=""
-                      required
-                    />
-                    <small class="text-muted">
-                      Full name as displayed on card
-                    </small>
-                    <div class="invalid-feedback">Name on card is required</div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <label for="cc-number">Credit card number</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="cc-number"
-                      placeholder=""
-                      required
-                    />
-                    <div class="invalid-feedback">
-                      Credit card number is required
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-3 mb-3">
-                    <label for="cc-expiration">Expiration</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="cc-expiration"
-                      placeholder=""
-                      required
-                    />
-                    <div class="invalid-feedback">Expiration date required</div>
-                  </div>
-                  <div class="col-md-3 mb-3">
-                    <label for="cc-expiration">CVV</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="cc-cvv"
-                      placeholder=""
-                      required
-                    />
-                    <div class="invalid-feedback">Security code required</div>
-                  </div>
-                </div>
-                <hr class="mb-4" />
+                
                 <button
                   class="btn btn-danger btn-md btn-block mb-3"
                   type="button"
