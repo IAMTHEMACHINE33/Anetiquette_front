cfc5fa1850ad0d70113e5f9fcfb6fd27e36ad2b8
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-12-28 21:35:39 +0545

    multi product image show integration

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -18,6 +18,7 @@ const Product = () => {
   const [bid, setBid] = useState();
   const [highest, setHighest] = useState();
   const [hname, setHname] = useState();
+  const [doto,setDoto]=useState([]);
 
 
   const [elapsedTime, setElapsedTime] = useState(100000);
@@ -141,13 +142,13 @@ const Product = () => {
     axios
       .get("http://localhost:4000/product/single/" + pid, config)
       .then((response) => {
-        console.log(response.data.data);
+        // console.log(response.data.data);
         setDetails(response.data.data);
-        console.log(response.data.data.type);
+        // console.log(response.data.data.type);
         setType(response.data.data.type);
         var m = 0;
         var n;
-        console.log(response.data.data.bid_info.length);
+        // console.log(response.data.data.bid_info.length);
         for (let i = 0; i < response.data.data.bid_info.length; i++) {
           if (m < response.data.data.bid_info[i].bid_price) {
             m = response.data.data.bid_info[i].bid_price;
@@ -156,29 +157,38 @@ const Product = () => {
             setHname(n);
           }
         }
+        console.log(response.data.data.images[0].name)
+        for(let o=0;o<response.data.data.images.length;o++){
+          doto.push("http://localhost:4000/" + response.data.data.images[o].name);
+          // console.log(doto)
+          // console.log("http://localhost:4000/" + response.data.data.images[o].name)
+          // console.log("e")
+        }
+        
+
       });
   }, []);
+  
+  // const data = [
+  //   "https://i0.wp.com/rachelfroud.com/wp-content/uploads/2020/06/Flamingo-Print-Mockup.jpg?fit=2305%2C2000&ssl=1",
+  //   "https://w0.peakpx.com/wallpaper/908/997/HD-wallpaper-one-piece-luffy-thumbnail.jpg",
+  //   img2,
+  //   img1,
+  //   "https://cdn.vox-cdn.com/thumbor/8wbWvZmEgMB1Ukx_A51_nfl-uCU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23929708/tekken_8.jpg",
 
-  const data = [
-    "https://i0.wp.com/rachelfroud.com/wp-content/uploads/2020/06/Flamingo-Print-Mockup.jpg?fit=2305%2C2000&ssl=1",
-    "https://w0.peakpx.com/wallpaper/908/997/HD-wallpaper-one-piece-luffy-thumbnail.jpg",
-    img2,
-    img1,
-    "https://cdn.vox-cdn.com/thumbor/8wbWvZmEgMB1Ukx_A51_nfl-uCU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23929708/tekken_8.jpg",
-
-    "https://c4.wallpaperflare.com/wallpaper/966/456/890/tekken-tag-tournament-2-jin-kazama-2560x1600-video-games-tekken-hd-art-wallpaper-preview.jpg",
-    "https://i.pinimg.com/736x/56/13/e7/5613e70ac2f1b34ccfbe8145dd7ad43a--virtual-games-tekken-.jpg",
-    "https://w0.peakpx.com/wallpaper/638/623/HD-wallpaper-akuma-capcom-street-fighter-videogames-thumbnail.jpg",
-    "https://p4.wallpaperbetter.com/wallpaper/621/399/620/chun-li-street-fighter-wallpaper-preview.jpg",
-    "https://i.pinimg.com/564x/c7/67/c9/c767c9ece36943979490b1a0d0db1126--ryu-street-fighter-nerd-art.jpg",
-    "https://i.pinimg.com/originals/f3/f6/e3/f3f6e357b08a50671dcee9adfbd7f04e.png",
-    "https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png",
-  ];
+  //   "https://c4.wallpaperflare.com/wallpaper/966/456/890/tekken-tag-tournament-2-jin-kazama-2560x1600-video-games-tekken-hd-art-wallpaper-preview.jpg",
+  //   "https://i.pinimg.com/736x/56/13/e7/5613e70ac2f1b34ccfbe8145dd7ad43a--virtual-games-tekken-.jpg",
+  //   "https://w0.peakpx.com/wallpaper/638/623/HD-wallpaper-akuma-capcom-street-fighter-videogames-thumbnail.jpg",
+  //   "https://p4.wallpaperbetter.com/wallpaper/621/399/620/chun-li-street-fighter-wallpaper-preview.jpg",
+  //   "https://i.pinimg.com/564x/c7/67/c9/c767c9ece36943979490b1a0d0db1126--ryu-street-fighter-nerd-art.jpg",
+  //   "https://i.pinimg.com/originals/f3/f6/e3/f3f6e357b08a50671dcee9adfbd7f04e.png",
+  //   "https://upload.wikimedia.org/wikipedia/en/2/2d/SSU_Kirby_artwork.png",
+  // ];
   const settings = {
     customPaging: (i) => {
       return (
         <div>
-          <img src={data[i]} alt="product.jpg" className="productsmall" />
+          <img src={doto[i]} alt="product.jpg" className="productsmall" />
         </div>
       );
     },
@@ -200,7 +210,7 @@ const Product = () => {
         <div className="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2  my-2 p-3">
           <div className="col container-fluid slider_container  p-2">
             <Slider {...settings}>
-              {data.map((item) => (
+              {doto.map((item) => (
                 <div>
                   <img
                     className="productmultimg"
